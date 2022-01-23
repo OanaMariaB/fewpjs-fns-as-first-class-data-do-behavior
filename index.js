d2faba15b4d0a5960bd1d20ac9078235aea90f7f
OanaMariaB <oana.berentan@yahoo.com>
2022-01-23 17:22:48 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet (timeString) {
+  const time = timeString.split(":").slice(0,1)
+  if (time < 12) {
+    return "Good Morning"
+  } else if (time >= 12 && time <= 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }  
+}
+
+function displayMessage(text){
+	document.getElementById("greeting").innerText= text;
+}
